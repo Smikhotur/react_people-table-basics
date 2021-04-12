6808f6bde3e76dd5101a4539e10c8fd3f66d70bc
Smihotur Roman <smikhotur@gmail.com>
2021-04-12 22:43:29 +0300

    Updates

---
+++ precache-manifest.106c933175a0011b62ee1207083cbcdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d549969dfb166a34facfa0a5ba80c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "20cde71c5c0a0ddb264f",
+    "url": "./static/css/main.0ea25473.chunk.css"
+  },
+  {
+    "revision": "05760314a8ab1b80624b",
+    "url": "./static/js/2.55ff12c9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.55ff12c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20cde71c5c0a0ddb264f",
+    "url": "./static/js/main.3a97df48.chunk.js"
+  },
+  {
+    "revision": "c7ae2510e9f54903a2ed",
+    "url": "./static/js/runtime-main.b51b247a.js"
+  }
+]);
